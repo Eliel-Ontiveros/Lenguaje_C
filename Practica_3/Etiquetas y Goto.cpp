47a3039a42ce868d63794a18f01f25be5f3f493e
Eliel-Ontiveros <eliel.ontiveros@uabc.edu.mx>
2023-09-13 23:48:16 -0700

    +

--- Practica_3/Etiquetas y Goto.cpp
+++ Practica_3/Etiquetas y Goto.cpp
@@ -6,67 +6,66 @@
 
 int main()
 {
-    inicio:
+    inicio: //Etiqueta para el inicio del juego
     printf("Bienvenidos a este juego para adividar un numero aleatorio\n");
 
-    adivinanza:
-    int control = 0;
-    int intentos = 1;
-    int num_sel;
-    int num;
-    int max = 100;
-    int min = 0;
-    int cant = 1;
-    int i;
+    adivinanza: //Etiqueta para el inicio de la adivinanza
+    int control = 0; //Variable de control para el bucle while
+    int intentos = 1; //Variable para contar los intentos del jugador
+    int num_sel; //Variable para contar los intentos del jugador
+    int num; //Variable para almacenar el número aleatorio generado por el programa
+    int max = 100; //Límite superior para el número aleatorio
+    int min = 0; //Límite inferior para el número aleatorio
+    int cant = 1; //Cantidad de números aleatorios a generar
+    int i; //Variable de iteración para el bucle for
     printf("Ingrese un numero entre el 1 y el 100\n");
-    scanf("%d", &num_sel);
-    for (i = 0; i < cant; i++)
+    scanf("%d", &num_sel); //Lectura del número seleccionado por el jugador
+    for (i = 0; i < cant; i++) // Bucle for para generar un número aleatorio
     {
-        num = (rand() % (max - min + 1) + min);
-        printf("Numero aleatorio = %d", num);
+        num = (rand() % (max - min + 1) + min); //Generación del número aleatorio
     }
-    while (control == 0)
+    while (control == 0) //Bucle while que se ejecuta hasta que el jugador adivine el número
     {
-        printf("\nIntento numero: %d\n", intentos);
+        printf("\nIntento numero: %d\n", intentos); //Impresión del número de intentos
         printf("\nIngrese un numero entre el 1 y el 100\n");
-        scanf("%d", &num_sel);
-        if (num_sel == num)
+        scanf("%d", &num_sel); //Lectura del número seleccionado por el jugador
+        if (num_sel == num) //Si el número seleccionado por el jugador es igual al número aleatorio...
         {
-            goto reinicio;
+            goto reinicio; //...el programa va a la etiqueta "reinicio"
         }
-        else
+        else //Si el número seleccionado por el jugador no es igual al número aleatorio...
         {
-            goto adivinanza;
+            goto adivinanza; //...el programa va a la etiqueta "adivinanza"
         }
 
     }
     
-    reinicio:
-    int op;
+    reinicio: //Etiqueta para el reinicio del juego
+    int op; // Variable para almacenar la opción seleccionada por el jugador para jugar de nuevo o no
     printf("\nQuieres volver a jugar?\n");
     printf("1 - Si\n");
     printf("2 - No\n");
-    scanf("%d", &op);
-    if (op == 1)
+    scanf("%d", &op); //Lectura de la opción seleccionada por el jugador
+    if (op == 1) //Si el jugador selecciona 1...
     {
-        goto adivinanza;
+        goto adivinanza; //...el programa va a la etiqueta "adivinanza"
     }
-    else
+    else 
     {
-        if (op == 2)
+        if (op == 2) //Si el jugador selecciona 2...
         {
-            printf("Muchas gracias por jugar\n");
+            printf("Muchas gracias por jugar\n"); //...el programa agradece al jugador y termina
 
             return 0;
         }
 
-        else
+        else //Si el jugador selecciona cualquier otro número...
         {
-            goto reinicio;
+            goto reinicio; //...el programa va a la etiqueta "reinicio"
         }
         
     }
 
-    goto inicio;
+    goto inicio; // Final del programa, pero nunca se alcanza debido a las declaraciones de retorno y goto anteriores
 
 }
