0fc5715f638b294d7aa5d2bf50b8cca0101021d7
Eliel-Ontiveros <eliel.ontiveros@uabc.edu.mx>
2023-09-30 03:18:35 -0700

    +

--- Taller_5/Variable_Global.cpp
+++ Taller_5/Variable_Global.cpp
@@ -20,9 +20,9 @@ Intenta acceder a ambas variables desde diferentes partes del programa. ¿Cual e
 En este codigo, la variable pi dentro de la funcion es una variable local y tiene precedencia sobre la variable global pi. 
 Por lo tanto, dentro de la funcion, el valor de pi sera 3.14. Fuera de la funcion, el valor de pi sera 3.14159.
 
-Explica el concepto de ámbito y visibilidad.
-El "Ambito" se refiere a la región del código donde una variable es visible y accesible. Determina la accesibilidad de las variables, 
-objetos, funciones y métodos en alguna parte particular del código durante el tiempo de ejecución.
-La "Visibilidad" se refiere a la propiedad de una variable, función, clase, interfaz, etc., de ser visible o accesible desde un determinado lugar en el código. 
-Controla si estas entidades pueden ser utilizadas o no en otras partes de tu código.
+Explica el concepto de ambito y visibilidad.
+El "Ambito" se refiere a la region del codigo donde una variable es visible y accesible. Determina la accesibilidad de las variables, 
+objetos, funciones y metodos en alguna parte particular del codigo durante el tiempo de ejecucion.
+La "Visibilidad" se refiere a la propiedad de una variable, funcion, clase, interfaz, etc., de ser visible o accesible desde un determinado lugar en el codigo. 
+Controla si estas entidades pueden ser utilizadas o no en otras partes de tu codigo.
 */
