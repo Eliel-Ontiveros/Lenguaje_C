46f60985597905390b076a010525726db00435ae
Eliel-Ontiveros <eliel.ontiveros@uabc.edu.mx>
2023-09-30 22:27:23 -0700

    +

--- Practicas/Practica_5/Practica_5.cpp
+++ Practicas/Practica_5/Practica_5.cpp
@@ -1,62 +1,62 @@
-#include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
+#include <stdio.h> // Incluye la biblioteca estándar de entrada y salida
+#include <time.h> // Incluye la biblioteca de tiempo para funciones relacionadas con el tiempo
+#include <stdlib.h> // Incluye la biblioteca estándar de la biblioteca
 
-void menu();
+void menu(); // Declaración de la función del menú
 
-int msgs();
+int msgs(); // Declaración de la función de mensajes
 
-int generar_num(void);
+int generar_num(void); // Declaración de la función para generar números
 
-void loteria(void);
+void loteria(void); // Declaración de la función de la lotería
 
-void carrera(void);
+void carrera(void); // Declaración de la función de la carrera
 
-typedef struct 
+typedef struct // Definición de la estructura del coche
 {
-    char nombre[10];
-    int velocidad;
-    float tiempo;
+    char nombre[10]; // Nombre del coche
+    int velocidad; // Velocidad del coche
+    float tiempo; // Tiempo que tarda el coche
 } Coche;
 
-void iniciarCarrera(Coche * coche);
+void iniciarCarrera(Coche * coche); // Declaración de la función para iniciar la carrera
 
-int main()
+int main() // Función principal
 {
-    menu();
+    menu(); // Llamada a la función del menú
 
-    return 0;
+    return 0; // Retorna 0 si el programa se ejecuta correctamente
 }
 
-int msgs()
+int msgs() // Función de mensajes
 {
-    int op;
-    system ("CLS");
+    int op; // Declaración de la variable de opción
+    system ("CLS"); // Limpia la pantalla
 
-    printf("------MENU------\n");
-    printf("\n1 - Simulacion de Juego de Loteria\n");
-    printf("2 - Simulacion de Carreras de Coches\n");
-    printf("0 - Salir del Programa\n");
-    scanf("%d", &op);
+    printf("------MENU------\n"); // Imprime el menú
+    printf("\n1 - Simulacion de Juego de Loteria\n"); // Opción de la lotería
+    printf("2 - Simulacion de Carreras de Coches\n"); // Opción de la carrera
+    printf("0 - Salir del Programa\n"); // Opción para salir del programa
+    scanf("%d", &op); // Lee la opción del usuario
 
-    return op;
+    return op; // Retorna la opción del usuario
 }
 
-void menu()
+void menu() // Función del menú
 {
-    int op;
+    int op; // Declaración de la variable de opción
     do
     {
-        op = msgs();
-        switch (op)
+        op = msgs(); // Llamada a la función de mensajes
+        switch (op) // Switch para las opciones
         {
         case 1:
-            loteria();
+            loteria(); // Llamada a la función de la lotería
 
             break;
 
         case 2:
-            carrera();
+            carrera(); // Llamada a la función de la carrera
 
             break;
         
@@ -64,82 +64,78 @@ void menu()
             break;
         }
     }
-    while (op != 0);
+    while (op != 0); // Mientras la opción no sea 0, el menú se seguirá mostrando
     
-    printf("Saliendo del Programa");
+    printf("Saliendo del Programa"); // Mensaje de salida del programa
     
 }
 
-int generar_num()
+int generar_num() // Función para generar números
 {
-    return rand() % 100 + 1;
+    return rand() % 100 + 1; // Retorna un número aleatorio entre 1 y 100
 }
 
-void loteria()
+void loteria() // Función de la lotería
 {
-    system ("CLS");
-    // Inicializar el generador de numeros aleatorios
-    srand(time(0));
+    system ("CLS"); // Limpia la pantalla
+    srand(time(0)); // Inicializa el generador de números aleatorios
 
-    // Inicializar el numero ganador solo una vez
-    static int numeroGanador = generar_num();
+    static int numeroGanador = generar_num(); // Inicializa el número ganador solo una vez
 
-    // Obtener un numero aleatorio para el jugador
-    int numeroJugador = generar_num();
+    int numeroJugador = generar_num(); // Obtiene un número aleatorio para el jugador
 
-    printf("Tu numero de loteria es: %d\n", numeroJugador);
+    printf("Tu numero de loteria es: %d\n", numeroJugador); // Imprime el número del jugador
 
-    // Comparar el numero del jugador con el numero ganador
-    if (numeroJugador == numeroGanador) 
+    if (numeroJugador == numeroGanador) // Compara el número del jugador con el número ganador
     {
-        printf("¡Felicidades, ganaste la loteria!\n");
+        printf("¡Felicidades, ganaste la loteria!\n"); // Mensaje de ganador
     } else 
     {
-        printf("Lo siento, no ganaste esta vez.\n");
+        printf("Lo siento, no ganaste esta vez.\n"); // Mensaje de perdedor
     }
 
-    system ("PAUSE");
+    system ("PAUSE"); // Pausa el programa
 }
 
 
-void iniciarCarrera(Coche * coche) 
+void iniciarCarrera(Coche * coche) // Función para iniciar la carrera
 {
     int distancia = 1000; // Distancia en metros
     coche->velocidad = 100 + rand() % 101; // Velocidad aleatoria entre 100 y 200
     coche->tiempo = (float)distancia / coche->velocidad; // Tiempo = distancia / velocidad
 }
 
-void carrera()
+void carrera() // Función de la carrera
 {
-    system ("CLS");
-    srand(time(0)); // Semilla para numeros aleatorios
+    system ("CLS"); // Limpia la pantalla
+    srand(time(0)); // Semilla para números aleatorios
 
-    Coche coche1 = {"Coche #1", 0, 0.0};
-    Coche coche2 = {"Coche #2", 0, 0.0};
-    Coche coche3 = {"Coche #3", 0, 0.0};
+    Coche coche1 = {"Coche #1", 0, 0.0}; // Inicialización del coche 1
+    Coche coche2 = {"Coche #2", 0, 0.0}; // Inicialización del coche 2
+    Coche coche3 = {"Coche #3", 0, 0.0}; // Inicialización del coche 3
 
-    iniciarCarrera(&coche1);
-    iniciarCarrera(&coche2);
-    iniciarCarrera(&coche3);
+    iniciarCarrera(&coche1); // Inicia la carrera para el coche 1
+    iniciarCarrera(&coche2); // Inicia la carrera para el coche 2
+    iniciarCarrera(&coche3); // Inicia la carrera para el coche 3
 
-    printf("%s viajo a %d km/h y tardo %.2f horas\n", coche1.nombre, coche1.velocidad, coche1.tiempo);
-    printf("%s viajo a %d km/h y tardo %.2f horas\n", coche2.nombre, coche2.velocidad, coche2.tiempo);
-    printf("%s viajo a %d km/h y tardo %.2f horas\n", coche3.nombre, coche3.velocidad, coche3.tiempo);
+    printf("%s viajo a %d km/h y tardo %.2f horas\n", coche1.nombre, coche1.velocidad, coche1.tiempo); // Imprime los resultados del coche 1
+    printf("%s viajo a %d km/h y tardo %.2f horas\n", coche2.nombre, coche2.velocidad, coche2.tiempo); // Imprime los resultados del coche 2
+    printf("%s viajo a %d km/h y tardo %.2f horas\n", coche3.nombre, coche3.velocidad, coche3.tiempo); // Imprime los resultados del coche 3
 
-    if (coche1.tiempo < coche2.tiempo && coche1.tiempo < coche3.tiempo) 
+    if (coche1.tiempo < coche2.tiempo && coche1.tiempo < coche3.tiempo) // Si el coche 1 es el más rápido
     {
-        printf("\n%s es el ganador!\n", coche1.nombre);
+        printf("\n%s es el ganador!\n", coche1.nombre); // Mensaje de ganador del coche 1
     } 
 
-    else if (coche2.tiempo < coche1.tiempo && coche2.tiempo < coche3.tiempo) 
+    else if (coche2.tiempo < coche1.tiempo && coche2.tiempo < coche3.tiempo) // Si el coche 2 es el más rápido
     {
-        printf("\n%s es el ganador!\n", coche2.nombre);
+        printf("\n%s es el ganador!\n", coche2.nombre); // Mensaje de ganador del coche 2
     } 
 
-    else 
+    else // Si el coche 3 es el más rápido
     {
-        printf("\n%s es el ganador!\n", coche3.nombre);
+        printf("\n%s es el ganador!\n", coche3.nombre); // Mensaje de ganador del coche 3
     }
 
-    system ("PAUSE");
+    system ("PAUSE"); // Pausa el programa
 }
